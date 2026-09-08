9683caf01c1fe763b1eabea6a3f40891f870131f
Lina Maria <lina.maria.c12345@hotmail.com>
2026-09-07 21:04:06 -0400

    Fix table initialization in app.py

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import boto3
 
 TABLE_NAME = os.environ.get('TABLE_NAME', 'NotesTable')
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(TABLE)
+table = dynamodb.Table()
 
 def response(status_code, body):
     return {
